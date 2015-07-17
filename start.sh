c134e570026e84900da6ed4434561183f0147688
Patrick Double <pat@patdouble.com>
2015-07-17 16:36:13 -0500

    Fix replacement of media folder

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ chown daapd /log/forked-daapd /cache/forked-daapd
 chmod u+rw /log/forked-daapd /cache/forked-daapd
 
 sed -i -e "s/name = \"Music Server\"/name = \"${MUSIC_SERVER_NAME:-Music Server}\"/" /etc/forked-daapd.conf
-sed -i -e "s/\/media/${MUSIC_SERVER_DATA:-\/media}/" /etc/forked-daapd.conf
+sed -i -e "s:/media:${MUSIC_SERVER_DATA:-/media}:" /etc/forked-daapd.conf
 
 /etc/init.d/dbus start
 /usr/sbin/avahi-daemon --no-chroot -D
