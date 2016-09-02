84855f0b8ec8d873f991e8a8754af6b1587acef2
Patrick Double <pat@patdouble.com>
2016-09-02 09:03:19 -0500

    Fix starting of dbus

--- start.sh
+++ start.sh
@@ -9,9 +9,10 @@ chmod -R u+rw /log/forked-daapd /cache/forked-daapd
 sed -i -e "s/name = \"Music Server\"/name = \"${MUSIC_SERVER_NAME:-Music Server}\"/" /etc/forked-daapd.conf
 sed -i -e "s:/media:${MUSIC_SERVER_DATA:-/media}:" /etc/forked-daapd.conf
 
-/etc/init.d/dbus start
+mkdir -p /var/run/dbus
+/usr/bin/dbus-daemon --system
 /usr/sbin/avahi-daemon --no-chroot -D
 /usr/sbin/forked-daapd -f -c /etc/forked-daapd.conf
 /usr/sbin/avahi-daemon -k
-/etc/init.d/dbus stop
+killall dbus-daemon
 
